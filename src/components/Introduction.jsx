77a20eb5eb671689c284cb7d0447ab7406057957
HarisZ <m.haris425@gmail.com>
2024-07-20 14:32:21 -0400

    Add Introduction Component

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Introduction(){
+    return(
+        <div className="intro-box container">
+            <p className="intro-text1">Ever since I was a kid, I have been fascinated by the world of computers. 
+                Driven by the limitless possibilities of programming, I began programming since high school.</p>
+                <br></br>
+            <p className="intro-text2">  
+                What began as a hobby turned into passion and since then I have worked on a number of personal projects.
+                As a current undergraduate computer science student, I spend time learning about machine learning and creating 
+                neural networks and models.
+                Other than that, I enjoy watching soccer and historical documentaries.
+                
+                </p>
+
+        </div>
+    )
+        
+    }
+
+    export default Introduction;
