64f0e6d3a97c09afa128c95b568f4650d47ff4c2
HarisZ <m.haris425@gmail.com>
2024-07-22 20:52:41 -0400

    Add Projects Section Component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,35 @@
+import projectsData from "./images/projects.json";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// ------- Project Images Import ----------- //
+import beige from "./images/beige-noise-bg.png";
+import calm from "./images/calm-bg.jpg";
+import golden from "./images/golden-noise-bg.png";
+
+const img = [beige, calm, golden]   //Array to hold the Project Images
+
+const Projects = () => {
+    const projects = projectsData.projects;
+    return(
+        <>
+        {projects.map((project, index) => (
+            
+            <div key={project.id} className="thePost container">
+                <Link to={'post/'+ project.id} className='post-link'>
+                <img className="post-img" src={img[index]}/>
+                <div className='post-title'>
+                    <h1>{project.title}</h1>
+                    <p>{project.description}</p>
+                </div>
+                </Link>
+            </div>
+ 
+
+))}
+</>
+)
+};
+
+
+export default Projects;
