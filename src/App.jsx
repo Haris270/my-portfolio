82ab934ac68853c8e0c3b89c6fdb5023e27ece90
HarisZ <m.haris425@gmail.com>
2024-07-24 18:12:23 -0400

    Add Routes & Components

--- src/App.jsx
+++ src/App.jsx
@@ -6,5 +6,24 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import ProjectDetails from './components/ProjectDetail';
-import ContactForm from './components/ContactForm';
+import Footer from './components/Footer';
+// import ContactForm from './components/ContactForm';
 
+function App(){
+    return(
+        <>
+            <Navbar/>
+            <div className='app'>
+                <Routes>
+                    <Route exact path='/' element={<HomePage/>}/>
+                    <Route path='/project/:id' element={<ProjectDetails/>}/>
+
+                </Routes>
+                
+            </div>
+            <Footer/>
+        </>
+    )
+}
+
+export default App;
