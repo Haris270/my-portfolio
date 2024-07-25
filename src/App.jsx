3a350e44aadae519486293650b33ea8f7c3faebf
HarisZ <m.haris425@gmail.com>
2024-07-25 12:05:45 -0400

    Add Contact Page Route

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import ProjectDetails from './components/ProjectDetail';
 import Footer from './components/Footer';
-// import ContactForm from './components/ContactForm';
+import ContactForm from './components/ContactForm';
 
 function App(){
     return(
@@ -17,6 +17,7 @@ function App(){
                 <Routes>
                     <Route exact path='/' element={<HomePage/>}/>
                     <Route path='/project/:id' element={<ProjectDetails/>}/>
+                    <Route path='/contact' element={<ContactForm/>}/>
 
                 </Routes>
                 
