3d5c85b5b69f8d19f08b13b77809b672f214d6e7
HarisZ <m.haris425@gmail.com>
2024-07-25 11:51:14 -0400

    Add Contact Form Component

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+
+const JContact = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        emailjs.send(
+            '***', //  EmailJS Service ID
+            '***', //  EmailJS Template ID
+            formData, 
+            '***' //   EmailJS User ID
+        )
+        .then((response) => {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Email sent successfully!');
+            setFormData({ name: '', email: '', message: '' });
+        })
+        .catch((err) => {
+            console.error('Failed to send email. Error: ', err);
+            alert('Failed to send email. Please try again later.');
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className='contact-text'>
+                <h1>Let's Get in Touch!</h1>
+            </div>
+            <div className='contact-boxes'>
+            <label>
+                Name:
+                <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+            </label>
+            <label>
+                Email:
+                <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+            </label>
+            <label>
+                Message:
+                <textarea name="message" value={formData.message} onChange={handleChange} required />
+            </label>
+            <button type="submit">Send Email</button>
+            </div>
+        </form>
+    );
+};
+
+export default JContact;
